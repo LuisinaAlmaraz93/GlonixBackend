5dd4084e7dc26194e06efcbea960de1533f604f9
fabricioalmaraz <fabricioalmaraz123@gmail.com>
2025-02-20 18:25:07 -0300

    Connect database

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Usa la variable de Render
-  ssl: { rejectUnauthorized: false } // Necesario para Render
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
-// Verificar conexión
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
-    console.error("❌ Error conectando a la base de datos:", err);
+    console.error("❌ Error conectando a la base de datos:", err.message, err.stack);
   } else {
     console.log("✅ Conexión exitosa a PostgreSQL. Hora actual:", res.rows[0].now);
   }
